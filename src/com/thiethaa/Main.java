4b2b1194153501126db3b9d750ed8140986272ef
atlroc99 <tyaniceluv89@gmail.com>
2020-10-08 20:42:36 -0400

    exam

---
+++ src/com/thiethaa/Main.java
@@ -0,0 +1,10 @@
+package com.thiethaa;
+
+public class Main {
+//find missing number of:
+    private static int[] arr = {4,5,3,4,2,5,1,7,4,5,2,3,4};
+    private static int[] arr2 = {5,5,5,6,4,4,1,3,8,1,8};
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
